10d9007be536952af0edbe02727d5332ded2ff3e
Leticia-23 <760549@unizar.es>
2022-12-11 14:44:59 +0200

    upload photo from postman working

--- adoptme-server/controllers/images.controller.js
+++ adoptme-server/controllers/images.controller.js
@@ -21,19 +21,13 @@ require("dotenv").config({ path: "./config.env" });
 const apiKey = process.env.IMGBB_KEY;
 
 const uploadImage = async (req, res) => {
-  console.log("req: ", req);
   const file = req.file;
-  console.log("file: ", file);
-
-  console.log("api_key: ", apiKey);
 
   imgbbUploader(apiKey, file.path)
     .then((response) => {
-      console.log(response);
       return res.status(201).json(response);
     })
     .catch((error) => {
-      console.error(error);
       return res.status(500).json({ error: error });
     });
 };
